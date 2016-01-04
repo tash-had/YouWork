2322f52d0de277d65de3d6131a0a8f88054380fb
tash-had <nanylagoon@gmail.com>
2016-01-03 23:22:21 -0500

    updated scripts

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     $("#loadMoreBtn").hide();
 });
 var ytObj = new apiheap("youtube", "AIzaSyCE1D-pcjpBzXa0iL60A4SUMDCgNS2uqy4"),
-    query_data = [],
-    loadMoreCount = 1;
+query_data = [],
+loadMoreCount = 1;
 
 function search() {
     var deferredObj = $.Deferred();
@@ -18,7 +18,23 @@ function search() {
 }
 
 var executeSearch = function() {
-    console.log(ytObj.parse(query_data, "vid_thumbnails"));
+    var titles = ytObj.parse(query_data, "vid_titles"); 
+    var ids = ytObj.parse(query_data, "vid_ids"); 
+    var descs = ytObj.parse(query_data, "vid_descriptions"); 
+    var channels = ytObj.parse(query_data, "chan_names"); 
+    var thumbs = ytObj.parse(query_data, "vid_thumbnails"); 
+    var amount = ids.length; 
+    var lb = "<br>"; 
+
+    for(x=0;x<amount;x++){
+        document.write(
+            lb+ 
+            '<img src="'+thumbs[x] +'" height=150 width=200 alt="Video Thumbnail"/>'  + lb + 
+            '<b>Video Title: </b>' + titles[x] + lb +
+           // '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ids[x]+'" frameborder="0" allowfullscreen></iframe>'+ lb + 
+            '<b>Video Description </b>' + descs[x] + lb + 
+            '<b>Channel Name: </b>' + channels[x] + lb + lb);
+    }
 }
 
 function loadMore() {
